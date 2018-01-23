95fc809b0fbb66ee12871337f56c89dff2b7fbbf
Stuck <Stuck@192.168.2.69>
2018-01-23 21:47:36 +0100

    update

--- src/main/conversion/SVGParseListener.java
+++ src/main/conversion/SVGParseListener.java
@@ -39,6 +39,35 @@ public class SVGParseListener extends SVGParserBaseListener {
 
 	@Override
 	public void exitCircle(CircleContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitCircle(ctx);
+
+		List<AttributeContext> list = ctx.attribute();
+
+		for (AttributeContext a : list) {
+			// System.out.println("Attribute Name: "+a.NAME());
+			// System.out.println("Attribute Value: "+a.STRING());
+
+			String r = "0";
+			String cx = "0";
+			String cy = "0";
+
+			if (a.NAME().toString() == "r") {
+				r = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "cx") {
+				cx = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "cy") {
+				cy = a.STRING().toString();
+			}
+
+			String path = "\\draw (" + cx + "," + cy + ") circle (" + r + ");";
+
+			tikzBuilder.appendString(path);
+		}
 
 	}
 
@@ -46,30 +75,171 @@ public class SVGParseListener extends SVGParserBaseListener {
 	public void exitRect(RectContext ctx) {
 		// TODO Auto-generated method stub
 		super.exitRect(ctx);
+
+		List<AttributeContext> list = ctx.attribute();
+
+		for (AttributeContext a : list) {
+
+			String x = "0";
+			String y = "0";
+			String width = "0";
+			String height = "0";
+
+			if (a.NAME().toString() == "x") {
+				x = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "y") {
+				y = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "width") {
+				width = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "height") {
+				height = a.STRING().toString();
+			}
+
+			String path = "\\draw (" + x + "," + y + ") -- (" + height + "," + y + ") -- (" + height + "," + width
+					+ ") -- (" + x + "," + width + ") -- (" + x + "," + y + ")";
+
+			tikzBuilder.appendString(path);
+		}
 	}
 
 	@Override
 	public void exitEllipse(EllipseContext ctx) {
 		// TODO Auto-generated method stub
 		super.exitEllipse(ctx);
+
+		List<AttributeContext> list = ctx.attribute();
+
+		for (AttributeContext a : list) {
+
+			String cx = "0";
+			String cy = "0";
+			String rx = "0";
+			String ry = "0";
+
+			if (a.NAME().toString() == "cx") {
+				cx = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "cy") {
+				cy = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "rx") {
+				rx = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "ry") {
+				ry = a.STRING().toString();
+			}
+
+			String path = "\\draw (" + cx + "," + cy + ") ellipse (" + rx + " and " + ry + ");";
+
+			tikzBuilder.appendString(path);
+		}
+
 	}
 
 	@Override
 	public void exitLine(LineContext ctx) {
 		// TODO Auto-generated method stub
 		super.exitLine(ctx);
+
+		List<AttributeContext> list = ctx.attribute();
+
+		for (AttributeContext a : list) {
+
+			String x1 = "0";
+			String y1 = "0";
+			String x2 = "0";
+			String y2 = "0";
+
+			if (a.NAME().toString() == "x1") {
+				x1 = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "y1") {
+				y1 = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "x2") {
+				x2 = a.STRING().toString();
+			}
+
+			if (a.NAME().toString() == "y2") {
+				y2 = a.STRING().toString();
+			}
+
+			String path = "\\draw (" + x1 + "," + y1 + ") -- (" + x2 + "," + y2 + ");";
+
+			tikzBuilder.appendString(path);
+		}
 	}
 
 	@Override
 	public void exitPolygon(PolygonContext ctx) {
 		// TODO Auto-generated method stub
 		super.exitPolygon(ctx);
+
+		// ??????
+
+		List<AttributeContext> list = ctx.attribute();
+
+		for (AttributeContext a : list) {
+
+			String points = "0,0 ";
+
+			if (a.NAME().toString() == "points") {
+				points = a.STRING().toString();
+			}
+
+			String[] point = points.split(" ");
+
+			String coordinates = "";
+
+			for (String coordinate : point) {
+				coordinates = coordinates + " (" + coordinate + ")";
+			}
+
+			String path = "\\draw plot[smooth, tension=2] coordinates { " + coordinates + "};";
+
+			tikzBuilder.appendString(path);
+		}
 	}
 
 	@Override
 	public void exitPolyline(PolylineContext ctx) {
 		// TODO Auto-generated method stub
 		super.exitPolyline(ctx);
+		
+		List<AttributeContext> list = ctx.attribute();
+		
+		for(AttributeContext a : list) {
+			
+			String points="0,0 ";
+			
+			if(a.NAME().toString()=="points"){
+				points=a.STRING().toString();
+			}
+			
+			String[] point = points.split(" ");
+			
+			String coordinates="";
+			
+			for(String coordinate : point) {
+				coordinates=coordinates+" ("+coordinate+")";
+			}
+			
+			
+			String path="\\draw plot[smooth, tension=2] coordinates { "+coordinates+"};";
+			
+			tikzBuilder.appendString(path);
+		}
 	}
 
 	@Override
@@ -92,8 +262,8 @@ public class SVGParseListener extends SVGParserBaseListener {
 						int R = Integer.parseInt(fillColor[1].substring(1, 3), 16);
 						int G = Integer.parseInt(fillColor[1].substring(3, 5), 16);
 						int B = Integer.parseInt(fillColor[1].substring(5, 7), 16);
-						//System.out.println(fillColor[1] +" -> "+R + "  " + G + "  " + B);
-						pathElem[0] = "\\definecolor{tmpcolor}{RGB}{"+R+","+G+","+B+"}";
+						// System.out.println(fillColor[1] +" -> "+R + " " + G + " " + B);
+						pathElem[0] = "\\definecolor{tmpcolor}{RGB}{" + R + "," + G + "," + B + "}";
 						break;
 					}
 				}
